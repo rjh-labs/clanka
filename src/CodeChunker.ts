2ce06c57626f13d7329166e0630919157b7e0e3b
Tim Smart <hello@timsmart.co>
2026-03-19 11:20:08 +1300

    fix

--- src/CodeChunker.ts
+++ src/CodeChunker.ts
@@ -534,7 +534,7 @@ const collectAstRanges = (
 
   try {
     const parser = new TreeSitter()
-    parser.setLanguage(language)
+    parser.setLanguage(language as TreeSitter.Language)
     const tree = parser.parse(content, undefined, {
       bufferSize: 1024 * 1024,
     })
